3e9ec7c688c8f939fb18159e0f6e3ca8334d2b67
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 19:30:29 +0700

    Percobaan mencetak literal string

---
+++ src/M_LiteralString.java
@@ -0,0 +1,6 @@
+public class M_LiteralString {
+    public static void main(String[] args) {
+        String welcome = "Pemograman Berorientasi Objek dengan Java";
+        System.out.println(welcome);
+    }
+}
