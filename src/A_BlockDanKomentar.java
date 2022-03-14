09bc9830fb56637554fe54ed618c2afaa499a965
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 14:54:36 +0700

    initial project pertemuan 1 dasar Java

---
+++ src/A_BlockDanKomentar.java
@@ -0,0 +1,14 @@
+public class A_BlockDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemograman Berorientasi Objek");
+        //contoh komentar 1 baris
+
+        /*
+        contoh komentar lebih dari satu baris
+         */
+
+        /**
+         * Java Doc --> baris ini mendokumentasikan kode program
+         */
+    }
+}
