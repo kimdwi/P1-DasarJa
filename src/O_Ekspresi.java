ce8134bc3ead90e79afb40ff339cbaa26daeada8
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 19:35:25 +0700

    Percobaan memahami ekspresi

---
+++ src/O_Ekspresi.java
@@ -0,0 +1,9 @@
+public class O_Ekspresi { //kombinasi dari operator, fungsi, nilai literal, variable atau konstanta dan selalu merubah nilai suatu variable
+    public static void main(String[] args) {
+        int bilA = 5;
+        int bilB = bilA * 2;
+        int bilC = bilA +bilB;
+        System.out.println("Ini contoh ekspresi : bilA * 2 = " +bilB);
+        System.out.println("Ini contoh ekspresi : bilA + bilB = " + bilC);
+    }
+}
