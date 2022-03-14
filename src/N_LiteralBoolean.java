7178b0828affcf6ca6e2104493a28e6fce6703a2
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 19:30:51 +0700

    Percobaan mencetak literal boolean

---
+++ src/N_LiteralBoolean.java
@@ -0,0 +1,8 @@
+public class N_LiteralBoolean {
+    public static void main(String[] args) {
+        boolean isOpen = true;
+        boolean isClose = false;
+        System.out.println("contoh literal boolean kondisi " + isOpen);
+        System.out.println("contoh literal boolean kondisi " + isClose);
+    }
+}
