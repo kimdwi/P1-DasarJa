d8efade34c6e5d0c2872c1cd93bca543c54e367e
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 20:04:10 +0700

    Percobaan mengkonversi nilai masukkan String dari Keyboard ke tipe tertentu

--- src/V_InputTypeScannerA.java
+++ src/V_InputTypeScannerA.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class V_InputTypeScanner {
+public class V_InputTypeScannerA {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan nama : ");
