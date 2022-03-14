580bd88dc387458eede2ec592e80372e12568ff5
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 19:25:52 +0700

    Percobaan mencetak nilai konstanta

---
+++ src/G_MencetakNilaiKonstanta.java
@@ -0,0 +1,9 @@
+public class G_MencetakNilaiKonstanta {
+    public static void main(String[] args) {
+        final double PHI = 3.14;
+        final double CM_PER_INCH = 2.54;
+        //final menunjukkan sebuah konstanta yang mana nama variable nya biasanya huruf besar seluruhnya dan nilai nya tidak bisa di ubah
+        System.out.println("Nilai Konstanta PHI = " + PHI);
+        System.out.println("Nilai konstanta CM_PER_INCH = " + CM_PER_INCH);
+    }
+}
