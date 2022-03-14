7139662ff695e3a7587e51ccdf234802b3fc38e2
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 19:29:14 +0700

    Percobaan mencetak literal character

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,14 @@
+public class K_LiteralChar {
+    public static void main(String[] args) {
+        char tab = '\t';
+        char newline = '\n';
+        char singleQuote = '\'';
+        char doubleQuote = '\"';
+        System.out.println("Ini contoh" + tab + "literal tab");
+        System.out.println("Ini contoh" + newline + "literal newline");
+        System.out.println("" + singleQuote + "Ini contoh single quote" + singleQuote);
+        System.out.println("" + doubleQuote + "Ini contoh literal double quote" + doubleQuote);
+
+
+    }
+}
