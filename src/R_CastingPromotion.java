1f26759c5c2f88e47f80e91998d6d70f3a9b297b
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 19:38:01 +0700

    Percobaan konversi tipe data

---
+++ src/R_CastingPromotion.java
@@ -0,0 +1,26 @@
+public class R_CastingPromotion {
+    public static void main(String[] args) {
+        long varLong = 1000000L;
+        int varInt=23456;
+        varInt = (int) varLong;
+        varLong = varInt;
+        System.out.println("Nilai varLong = " + varLong);
+        System.out.println("Nilai varint = "+ varInt);
+        char varChar = 'a';
+        varInt= (int) varChar;
+        System.out.println("Nilai varint ('a') = "+ varInt);
+        varChar = (char) (varInt+1);
+        System.out.println("Nilai varChar = "+ varChar);
+        byte varByte = 127;
+        varInt = varByte;
+        System.out.println("Nilai varint (127) = " + varInt);
+        short varShort = (short) varInt;
+        System.out.println("Nilai varShort = "+ varShort);
+        varShort = varByte;
+        System.out.println("Nilai varShort = "+ varShort);
+        double varDouble = 3.145256718992022;
+        float varFloat= (float) varDouble;
+        System.out.println("Nilai varDouble = " + varDouble);
+        System.out.println("Nilai varFloat = " + varFloat);
+    }
+}
