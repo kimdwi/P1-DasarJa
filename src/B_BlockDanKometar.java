13011690f71d00292c84941f9e9fda743cfd3552
kimdwi <parkdwiputriani@gmail.com>
2022-03-14 15:18:13 +0700

    Percobaan whitespace di Java

---
+++ src/B_BlockDanKometar.java
@@ -0,0 +1,4 @@
+/**
+ * Contoh penggunaan whitespace di Java
+ */
+public class B_BlockDanKometar { public static void main(String[] args) { System.out.println("Program Berorientasi Objek dengan Java"); }}
